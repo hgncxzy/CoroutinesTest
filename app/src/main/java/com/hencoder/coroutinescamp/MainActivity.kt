1764a58dedb07f01e2890c7782aa6f5be586daa8
hgncxzy <hgncxzy@qq.com>
2020-06-24 17:31:33 +0800

    refactor code

--- app/src/main/java/com/hencoder/coroutinescamp/MainActivity.kt
+++ app/src/main/java/com/hencoder/coroutinescamp/MainActivity.kt
@@ -32,41 +32,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.setOnClickListener {
-            startActivity(Intent(this, PracticeActivity1::class.java))
-        }
-
-        // 直接使用 Thread 切线程
-        Thread {
-            println("test01,Thread io1 ${Thread.currentThread().name}")
-            runOnUiThread {
-                println("test01,Thread ui1 ${Thread.currentThread().name}")
-            }
-        }.start()
-
-        // 直接使用 Thread 切线程
-        thread {
-            println("test02,Thread io2 ${Thread.currentThread().name}")
-            runOnUiThread {
-                println("test02,Thread ui2 ${Thread.currentThread().name}")
-            }
-        }
-
-        // 启动一个协程
-        GlobalScope.launch {
-            println("Coroutines Camp 0 ${Thread.currentThread().name}")
-        }
-
-        // 使用协程启动 io 代码和 ui 代码
-        GlobalScope.launch(Dispatchers.Main) {
-            ioCode1()
-            uiCode1()
-            ioCode2()
-            uiCode2()
-            ioCode3()
-            uiCode3()
-        }
-
         // 初始化 retrofit
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
@@ -76,80 +41,143 @@ class MainActivity : AppCompatActivity() {
 
         val api = retrofit.create(Api::class.java)
 
-        // 使用接口回调方式依次调用两个 io 接口
-        api.listRepos("rengwuxian")
-            .enqueue(object : Callback<List<Repo>?> {
-                override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
 
+        textView.setOnClickListener {
+            startActivity(Intent(this, PracticeActivity1::class.java))
+        }
+        // 直接使用 Thread 切线程
+        btn1.setOnClickListener {
+            Thread {
+                println("xzy test01,Thread io1 ${Thread.currentThread().name}")
+                runOnUiThread {
+                    println("xzy test01,Thread ui1 ${Thread.currentThread().name}")
                 }
-
-                override fun onResponse(call: Call<List<Repo>?>, response: Response<List<Repo>?>) {
-                    // 第一次请求后的回调结果
-                    val response1 = response.body()?.get(0)?.name
-                    // 接着执行第二次请求
-                    api.listRepos("google")
-                        .enqueue(object : Callback<List<Repo>?> {
-                            override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
-
-                            }
-
-                            override fun onResponse(
-                                call: Call<List<Repo>?>,
-                                response: Response<List<Repo>?>
-                            ) {
-                                // 在第二次的请求回调中合并结果并更新 ui
-                                val response2 = response.body()?.get(0)?.name
-                                textView.text = response1 + response2
-                            }
-                        })
-                }
-            })
-
-        // 使用协程 + retrofit 启动 io 代码 A,和 io 代码 B,并将结果合并后更新 ui
-        GlobalScope.launch(Dispatchers.Main) {
-            val rengwuxian = async { api.listReposKt("rengwuxian") }
-            val google = async { api.listReposKt("google") }
-            textView.text = "${rengwuxian.await()[0].name} + ${google.await()[0].name}"
+            }.start()
         }
 
-        // 使用 ktx 工具箱封装的协程请求 io 接口
-        lifecycleScope.launch {
-            try {
-                // 正常请求接口，
-                val repos: List<Repo> = api.listReposKt("rengwuxian")
-                textView.text = repos[0].name + "-kt"
-            } catch (e: Exception) {
-                textView.text = e.message
+        // 直接使用 Thread 切线程
+        btn2.setOnClickListener {
+            thread {
+                println("xzy test02,Thread io2 ${Thread.currentThread().name}")
+                runOnUiThread {
+                    println("xzy test02,Thread ui2 ${Thread.currentThread().name}")
+                }
             }
         }
 
-        // 使用 RxJava + 协程请求多个 io 接口，并合并结果，更新 ui
-        Single.zip<List<Repo>, List<Repo>, String>(
-            api.listReposRx("rengwuxian"),
-            api.listReposRx("google"),
-            BiFunction { repos1, repos2 -> "${repos1[0].name} - ${repos2[0].name}" }
-        ).observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<String> {
-                override fun onSuccess(combined: String) {
-                    textView.text = combined
-                }
+        // 使用接口回调方式依次调用两个 io 接口
+        btn3.setOnClickListener {
+            api.listRepos("rengwuxian")
+                .enqueue(object : Callback<List<Repo>?> {
+                    override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
 
-                override fun onSubscribe(d: Disposable) {
-                    disposable.add(d)
-                }
+                    }
 
-                override fun onError(e: Throwable) {
+                    override fun onResponse(
+                        call: Call<List<Repo>?>,
+                        response: Response<List<Repo>?>
+                    ) {
+                        // 第一次请求后的回调结果
+                        val response1 = response.body()?.get(0)?.name
+                        println("使用接口回调方式依次调用两个 io 接口，第一次请求后的回调结果：$response1")
+                        // 接着执行第二次请求
+                        api.listRepos("google")
+                            .enqueue(object : Callback<List<Repo>?> {
+                                override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
+
+                                }
+
+                                override fun onResponse(
+                                    call: Call<List<Repo>?>,
+                                    response: Response<List<Repo>?>
+                                ) {
+                                    // 在第二次的请求回调中合并结果并更新 ui
+                                    val response2 = response.body()?.get(0)?.name
+                                    textView.text = response1 + response2
+                                    println("使用接口回调方式依次调用两个 io 接口，在第二次的请求回调中合并结果并更新 ui：${response1 + response2}")
+                                }
+                            })
+                    }
+                })
+        }
+
+        // 启动一个协程
+        btn4.setOnClickListener {
+            GlobalScope.launch {
+                println("xzy Coroutines Camp 0 ${Thread.currentThread().name}")
+            }
+        }
+
+        // 使用协程启动 io 代码和 ui 代码
+        btn5.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+                ioCode1()
+                uiCode1()
+                ioCode2()
+                uiCode2()
+                ioCode3()
+                uiCode3()
+            }
+        }
+
+        // 使用协程 + retrofit 启动 io 代码 A,和 io 代码 B,并将结果合并后更新 ui
+        btn6.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+                val rengwuxian = async { api.listReposKt("rengwuxian") }
+                val google = async { api.listReposKt("google") }
+                textView.text = "${rengwuxian.await()[0].name} + ${google.await()[0].name}"
+            }
+        }
+
+        // 使用 ktx 工具箱封装的协程请求 io 接口
+        btn7.setOnClickListener {
+            lifecycleScope.launch { // 还有 viewModelScope 等
+                try {
+                    // 正常请求接口，
+                    val repos: List<Repo> = api.listReposKt("rengwuxian")
+                    textView.text = repos[0].name + "-kt"
+                } catch (e: Exception) {
                     textView.text = e.message
                 }
-            })
-        // 使用 executor 请求 io 操作
-        classicIoCode1 {
-            uiCode1()
+            }
+        }
+
+
+        // 使用 RxJava + 协程请求多个 io 接口，并合并结果，更新 ui
+        btn8.setOnClickListener {
+            Single.zip<List<Repo>, List<Repo>, String>(
+                api.listReposRx("rengwuxian"),
+                api.listReposRx("google"),
+                BiFunction { repos1, repos2 -> "${repos1[0].name} - ${repos2[0].name}" }
+            ).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : SingleObserver<String> {
+                    override fun onSuccess(combined: String) {
+                        textView.text = combined
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                        disposable.add(d)
+                    }
+
+                    override fun onError(e: Throwable) {
+                        textView.text = e.message
+                    }
+                })
         }
 
         // 使用 executor 请求 io 操作
-        classicIoCode1(false) {
-            uiCode1()
+        btn9.setOnClickListener {
+            classicIoCode1 {
+                uiCode1()
+            }
+        }
+
+
+        // 使用 executor 请求 io 操作
+        btn10.setOnClickListener {
+            classicIoCode1(false) {
+                uiCode1()
+            }
         }
     }
 
@@ -158,7 +186,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun classicIoCode1(uiThread: Boolean = true, block: () -> Unit) {
         executor.execute {
-            println("Coroutines Camp classic io1 ${Thread.currentThread().name}")
+            println("xzy executor classic io1 ${Thread.currentThread().name}")
             if (uiThread) {
                 runOnUiThread {
                     block()
@@ -176,31 +204,31 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun ioCode1() {
         withContext(Dispatchers.IO) {
-            println("Coroutines Camp io1 ${Thread.currentThread().name}")
+            println("xzy Coroutines Camp io1 ${Thread.currentThread().name}")
         }
     }
 
     private suspend fun ioCode2() {
         withContext(Dispatchers.IO) {
-            println("Coroutines Camp io2 ${Thread.currentThread().name}")
+            println("xzy Coroutines Camp io2 ${Thread.currentThread().name}")
         }
     }
 
     private suspend fun ioCode3() {
         withContext(Dispatchers.IO) {
-            println("Coroutines Camp io3 ${Thread.currentThread().name}")
+            println("xzy Coroutines Camp io3 ${Thread.currentThread().name}")
         }
     }
 
     private fun uiCode1() {
-        println("Coroutines Camp ui1 ${Thread.currentThread().name}")
+        println("xzy Coroutines Camp ui1 ${Thread.currentThread().name}")
     }
 
     private fun uiCode2() {
-        println("Coroutines Camp ui2 ${Thread.currentThread().name}")
+        println("xzy Coroutines Camp ui2 ${Thread.currentThread().name}")
     }
 
     private fun uiCode3() {
-        println("Coroutines Camp ui3 ${Thread.currentThread().name}")
+        println("xzy Coroutines Camp ui3 ${Thread.currentThread().name}")
     }
 }
