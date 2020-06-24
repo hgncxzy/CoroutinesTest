03de0dfbcc7222d784351f1a5bc7ed3d9544aff2
hgncxzy <hgncxzy@qq.com>
2020-06-24 14:40:09 +0800

    init

---
+++ app/src/main/java/com/hencoder/coroutinescamp/MainActivity.kt
@@ -0,0 +1,206 @@
+package com.hencoder.coroutinescamp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.hencoder.coroutinescamp.model.Repo
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.SingleObserver
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.BiFunction
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import retrofit2.*
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    val disposable = CompositeDisposable()
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener {
+            startActivity(Intent(this, PracticeActivity1::class.java))
+        }
+
+        // 直接使用 Thread 切线程
+        Thread {
+            println("test01,Thread io1 ${Thread.currentThread().name}")
+            runOnUiThread {
+                println("test01,Thread ui1 ${Thread.currentThread().name}")
+            }
+        }.start()
+
+        // 直接使用 Thread 切线程
+        thread {
+            println("test02,Thread io2 ${Thread.currentThread().name}")
+            runOnUiThread {
+                println("test02,Thread ui2 ${Thread.currentThread().name}")
+            }
+        }
+
+        // 启动一个协程
+        GlobalScope.launch {
+            println("Coroutines Camp 0 ${Thread.currentThread().name}")
+        }
+
+        // 使用协程启动 io 代码和 ui 代码
+        GlobalScope.launch(Dispatchers.Main) {
+            ioCode1()
+            uiCode1()
+            ioCode2()
+            uiCode2()
+            ioCode3()
+            uiCode3()
+        }
+
+        // 初始化 retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+
+        val api = retrofit.create(Api::class.java)
+
+        // 使用接口回调方式依次调用两个 io 接口
+        api.listRepos("rengwuxian")
+            .enqueue(object : Callback<List<Repo>?> {
+                override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
+
+                }
+
+                override fun onResponse(call: Call<List<Repo>?>, response: Response<List<Repo>?>) {
+                    // 第一次请求后的回调结果
+                    val response1 = response.body()?.get(0)?.name
+                    // 接着执行第二次请求
+                    api.listRepos("google")
+                        .enqueue(object : Callback<List<Repo>?> {
+                            override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
+
+                            }
+
+                            override fun onResponse(
+                                call: Call<List<Repo>?>,
+                                response: Response<List<Repo>?>
+                            ) {
+                                // 在第二次的请求回调中合并结果并更新 ui
+                                val response2 = response.body()?.get(0)?.name
+                                textView.text = response1 + response2
+                            }
+                        })
+                }
+            })
+
+        // 使用协程 + retrofit 启动 io 代码 A,和 io 代码 B,并将结果合并后更新 ui
+        GlobalScope.launch(Dispatchers.Main) {
+            val rengwuxian = async { api.listReposKt("rengwuxian") }
+            val google = async { api.listReposKt("google") }
+            textView.text = "${rengwuxian.await()[0].name} + ${google.await()[0].name}"
+        }
+
+        // 使用 ktx 工具箱封装的协程请求 io 接口
+        lifecycleScope.launch {
+            try {
+                // 正常请求接口，
+                val repos: List<Repo> = api.listReposKt("rengwuxian")
+                textView.text = repos[0].name + "-kt"
+            } catch (e: Exception) {
+                textView.text = e.message
+            }
+        }
+
+        // 使用 RxJava + 协程请求多个 io 接口，并合并结果，更新 ui
+        Single.zip<List<Repo>, List<Repo>, String>(
+            api.listReposRx("rengwuxian"),
+            api.listReposRx("google"),
+            BiFunction { repos1, repos2 -> "${repos1[0].name} - ${repos2[0].name}" }
+        ).observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : SingleObserver<String> {
+                override fun onSuccess(combined: String) {
+                    textView.text = combined
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                    disposable.add(d)
+                }
+
+                override fun onError(e: Throwable) {
+                    textView.text = e.message
+                }
+            })
+        // 使用 executor 请求 io 操作
+        classicIoCode1 {
+            uiCode1()
+        }
+
+        // 使用 executor 请求 io 操作
+        classicIoCode1(false) {
+            uiCode1()
+        }
+    }
+
+    // 使用 executor 请求 io 操作
+    private val executor = ThreadPoolExecutor(5, 20, 1, TimeUnit.MINUTES, LinkedBlockingDeque())
+
+    private fun classicIoCode1(uiThread: Boolean = true, block: () -> Unit) {
+        executor.execute {
+            println("Coroutines Camp classic io1 ${Thread.currentThread().name}")
+            if (uiThread) {
+                runOnUiThread {
+                    block()
+                }
+            } else {
+                block()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
+    }
+
+    private suspend fun ioCode1() {
+        withContext(Dispatchers.IO) {
+            println("Coroutines Camp io1 ${Thread.currentThread().name}")
+        }
+    }
+
+    private suspend fun ioCode2() {
+        withContext(Dispatchers.IO) {
+            println("Coroutines Camp io2 ${Thread.currentThread().name}")
+        }
+    }
+
+    private suspend fun ioCode3() {
+        withContext(Dispatchers.IO) {
+            println("Coroutines Camp io3 ${Thread.currentThread().name}")
+        }
+    }
+
+    private fun uiCode1() {
+        println("Coroutines Camp ui1 ${Thread.currentThread().name}")
+    }
+
+    private fun uiCode2() {
+        println("Coroutines Camp ui2 ${Thread.currentThread().name}")
+    }
+
+    private fun uiCode3() {
+        println("Coroutines Camp ui3 ${Thread.currentThread().name}")
+    }
+}
---
+++ app/src/main/java/com/hencoder/coroutinescamp/PracticeActivity1.kt
@@ -0,0 +1,37 @@
+package com.hencoder.coroutinescamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+class PracticeActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice1)
+
+        /*val data = getData()
+        val processedData = processData(data)*/
+        // 练习内容：用协程让上面 ↑ 这两行放在后台执行，然后把代码截图贴到腾讯课堂的作业
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = async { getData() }
+            val processedData = async { processData(data.await()) }
+            textView.text = processedData.await()
+        }
+//        textView.text = processedData
+    }
+
+    // 耗时函数 1
+    private fun getData(): String {
+        // 假设这个函数比较耗时，需要放在后台
+        return "hen_coder"
+    }
+
+    // 耗时函数 2
+    private fun processData(data: String): String {
+        // 假设这个函数也比较耗时，需要放在后台
+        return data.split("_") // 把 "hen_coder" 拆成 ["hen", "coder"]
+            .map { it.capitalize() } // 把 ["hen", "coder"] 改成 ["Hen", "Coder"]
+            .reduce { acc, s -> acc + s } // 把 ["Hen", "Coder"] 改成 "HenCoder"
+    }
+}
